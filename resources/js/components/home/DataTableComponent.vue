dda8523401891299d1e95b57c4bf1d697a4bd99c
Dino Cajic <dinocajic@gmail.com>
2022-12-16 12:32:56 -0500

    Laravel Vue DataTables

---
+++ resources/js/components/home/DataTableComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="p-6">
+        <DataTable
+            class="display"
+            :columns="columns"
+            ajax="api/users"
+            ref="table"
+            :options="{
+                select: true,
+                serverSide: true,
+            }"
+        >
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </DataTable>
+    </div>
+</template>
+
+<script>
+import DataTable from 'datatables.net-vue3'
+import DataTablesLib from 'datatables.net';
+
+DataTable.use(DataTablesLib);
+
+export default {
+    name: 'DataTableComponent',
+    components: {DataTable},
+    data(){
+        return {
+            columns: [
+                {"data": "id"},
+                {"data": "name"},
+                {"data": "email"},
+                {"data": "action", "sortable": false},
+            ],
+        }
+    },
+}
+</script>
+
+<style>
+@import 'datatables.net-dt';
+</style>
---
+++ resources/views/home/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Home Page with DataTable</h1>
+    <DataTable />
+</template>
+
+<script>
+import DataTable from "../../js/components/home/DataTableComponent.vue";
+
+export default {
+    name: 'Home',
+    components: {DataTable},
+}
+</script>
+
+<style>
+
+</style>
